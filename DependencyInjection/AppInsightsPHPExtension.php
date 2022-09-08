86efeb94957fcc9a5acd33da35870dc68d9d466a
arturLitvinavicius <94920089+arturLitvinavicius@users.noreply.github.com>
2022-09-08 12:54:56 +0300

    Update AppInsightsPHPExtension.php

--- DependencyInjection/AppInsightsPHPExtension.php
+++ DependencyInjection/AppInsightsPHPExtension.php
@@ -2,7 +2,16 @@
 
 declare(strict_types=1);
 
-namespace App\Azure\ApplicationInsights\Extension;
+/*
+ * This file is part of the App Insights PHP project.
+ *
+ * (c) Norbert Orzechowicz <norbert@orzechowicz.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppInsightsPHP\Symfony\AppInsightsPHPBundle\DependencyInjection;
 
 use App\Azure\ApplicationInsights\Handler\AzureAppInsightsTraceHandler;
 use AppInsightsPHP\Client\Client;
